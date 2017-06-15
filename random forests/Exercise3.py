ebac5efe7b6c2c6be6da1d77df8c02754455ef10
Shoaib Khan <mshoaibkhan91@gmail.com>
2017-06-15 18:04:08 +0200

    decision boundary visualization with k fold cross validation data sets

--- random forests/Exercise3.py
+++ random forests/Exercise3.py
@@ -3,7 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import ensemble
 from sklearn import model_selection
-from matplotlib.colors import ListedColormap
 
 class Util:
 
@@ -16,7 +15,7 @@ class Util:
         return np.loadtxt(path + fileName, delimiter=',', skiprows=1)
 
 
-class DataLoader:
+class RForestVisualizer:
     data = 0        #holds data that is loaded i.e spiral data or twistData
     trainSet = 0
     testSet = 0
@@ -60,67 +59,75 @@ class DataLoader:
         plt.scatter(X[:,0], X[:,1], c=Z, cmap=plt.cm.get_cmap("cool"))
         plt.show()
 
-
-    def visualizeLearning(self, X, X_train, y_train, X_test, y_test, clf):
+    # visualize the data points and draw contour graph
+    # X : data with 3 features X1, X2, Y
+    # kFolds : array of length k of tuples (train_Data, test_Data)
+    # clf : classifier
+    def visualizeLearningKfold(self,X, kFolds, clf):
         h = 0.02
-        plt.figure(figsize=(10, 5))
+        no_of_folds = len(kFolds)
+        plt.figure(figsize=(20, 10))
         x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
         y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
         xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
         cm = plt.cm.RdBu
         #cm_bright = ListedColormap(['#FF0000', '#0000FF'])
         cm_bright = plt.cm.cool
-        ax = plt.subplot(1, 2, 1)
-        ax.set_title("Random Forest")
-        # Plot the training points
-        ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright)
-        # and testing points
-        ax.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright, alpha=0.6)
-        ax.set_xlim(xx.min(), xx.max())
-        ax.set_ylim(yy.min(), yy.max())
-        ax.set_xticks(())
-        ax.set_yticks(())
 
-        # ---- plotting diecision boundaries
-        ax = plt.subplot(1, 2, 2)
-        clf.fit(X_train, y_train)
-        score = clf.score(X_test, y_test)
+        plotId, kfoldNo = 1, 1
+        for dset in kFolds:
+            dtrain, dtest = dset[0], dset[1]
+            X_train, X_test = dtrain[:, 0:2], dtest[:, 0:2]
+            y_train, y_test = dtrain[:,-1], dtest[:, -1]
 
-        # Plot the decision boundary. For that, we will assign a color to each
-        # point in the mesh [x_min, x_max]x[y_min, y_max].
-        if hasattr(clf, "decision_function"):
-            Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
-        else:
-            Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
-        # Put the result into a color plot
-        Z = Z.reshape(xx.shape)
-        ax.contourf(xx, yy, Z, cmap=cm, alpha=.8)
+            ax = plt.subplot(no_of_folds, 2, plotId)
+            ax.set_title("Fold : {}".format(kfoldNo))
+            # Plot the training points
+            ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright)
+            # and testing points
+            ax.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright, alpha=0.6)
+            ax.set_xlim(xx.min(), xx.max())
+            ax.set_ylim(yy.min(), yy.max())
+            ax.set_xticks(())
+            ax.set_yticks(())
 
-        # Plot also the training points
-        ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright)
-        # and testing points
-        ax.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright,alpha=0.6)
+            # ---- plotting diecision boundaries-------------
+            ax = plt.subplot(no_of_folds, 2, plotId+1)
+            clf.fit(X_train, y_train)
+            score = clf.score(X_test, y_test)
 
-        ax.set_xlim(xx.min(), xx.max())
-        ax.set_ylim(yy.min(), yy.max())
-        ax.set_xticks(())
-        ax.set_yticks(())
-        ax.set_title("Random Forest")
-        ax.text(xx.max() - .3, yy.min() + .3, ('%.2f' % score).lstrip('0'),
-                size=15, horizontalalignment='right')
+            # Plot the decision boundary. For that, we will assign a color to each
+            # point in the mesh [x_min, x_max]x[y_min, y_max].
+            if hasattr(clf, "decision_function"):
+                Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()])
+            else:
+                Z = clf.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
+            # Put the result into a color plot
+            Z = Z.reshape(xx.shape)
+            ax.contourf(xx, yy, Z, cmap=cm, alpha=.8)
+
+            # Plot also the training points
+            ax.scatter(X_train[:, 0], X_train[:, 1], c=y_train, cmap=cm_bright)
+            # and testing points
+            ax.scatter(X_test[:, 0], X_test[:, 1], c=y_test, cmap=cm_bright,alpha=0.6)
+
+            ax.set_xlim(xx.min(), xx.max())
+            ax.set_ylim(yy.min(), yy.max())
+            ax.set_xticks(())
+            ax.set_yticks(())
+            ax.set_title("contour for fold {}".format(kfoldNo))
+            ax.text(xx.max() - .3, yy.min() + .3, ('%.2f' % score).lstrip('0'),
+                    size=15, horizontalalignment='right')
+            plotId += 2
+            kfoldNo +=1
 
         plt.show()
 
-    def mapper(a):
-        if a == 0.2:
-            a = 2
-        elif a>=0 and a < 1:
-            a = 3
-        else: a = 1
-        return a
+
 
 if __name__ == "__main__":
-    dl = DataLoader()
+    dl = RForestVisualizer()
+
     # Switch data from here
     #dl.loadSpiralData()
     dl.loadTwistData()
@@ -128,9 +135,6 @@ if __name__ == "__main__":
     # partition data into 2 folds
     dl.createFoldsSklearn(2)
 
-    train1 = dl.kFolds[0][0]            #1st index: k in kfolds, 2nd index: 0 for train, 1 for test
-    test1 = dl.kFolds[0][1]
-
     forest10 = ensemble.RandomForestClassifier(10, max_depth=3)
     print("visualizing")
-    dl.visualizeLearning(dl.data, train1[:,0:2], train1[:,-1], test1[:,0:2], test1[:,-1], forest10)
+    dl.visualizeLearningKfold(dl.data, dl.kFolds, forest10)
