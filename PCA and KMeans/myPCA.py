3fa0e9f3e81a874e31ee01412df18f65b2b50084
Shoaib Khan <mshoaibkhan91@gmail.com>
2017-06-24 19:24:17 +0200

    pca with covariance matrix

--- PCA and KMeans/myPCA.py
+++ PCA and KMeans/myPCA.py
@@ -43,11 +43,11 @@ def usingSVD(dataMatrix, desiredVariancePercentage=1.0):
 
     transposedData = obj['demeanedDataMatrix'].T
     U, s, V = np.linalg.svd(transposedData, full_matrices=False)
-    print(U.shape, V.shape , s.shape)
+    #print(U.shape, V.shape , s.shape)
     S = np.diag(s)
+
     X_a = np.dot(np.dot(U, S), V)
     X_b = np.dot(np.dot(U[:,0:-1], S[0:2,0:2]), V[0:-1,:])
-
     X_c = np.dot(np.dot(U[:,0].reshape(U.shape[0],1), S[0,0].reshape(1,1)), V[0,:].reshape(1, V.shape[1]))
 
     print("standard deviation: original | transformed | difference")
@@ -58,11 +58,11 @@ def usingSVD(dataMatrix, desiredVariancePercentage=1.0):
     # Enforce a sign convention on the coefficients -- the largest element (absolute) in each
     # column will have a positive sign.
     maxVals = np.amax(X_b, axis=0)
-    print(maxVals)      # max vals are positive
+    #print(maxVals)      # max vals are positive
 
     obj['X_a'] = X_a
-    obj['X_b'] = X_b
-    obj['X_c'] = X_c
+    obj['X_b'] = X_b        # projection on 2 principle components
+    obj['X_c'] = X_c        # projection on 1 principle component
 
     # Compute the accumelative Eigenvalues to finde the desired
     # Variance
@@ -79,8 +79,8 @@ def usingSVD(dataMatrix, desiredVariancePercentage=1.0):
     
     
     # Project the data
-    obj['projectedData'] = np.dot(transposedData, eigvecs[:,0:2])
-    
+    obj['projectedData'] = np.dot(transposedData, eigvecs[:,0:2])       #TODO: this projection is not correct eventhough eigen vectors are correct. look into it
+                                                                        # this is because (100x3) x (3x2) = (100x2) so the z component is completely gone
     # return the object
     return obj
 
@@ -100,41 +100,54 @@ def usingSVD(dataMatrix, desiredVariancePercentage=1.0):
 
  
 def usingCOV(dataMatrix, desiredVariancePercentage=1.0):
-    pass
     # This function should implement the PCA using the
     # EigenValue Decomposition of a given Covariance Matrix 
-    
-    # De-Meaning the feature space 
-    #obj.meanDataMatrix =
-    #obj.demeanedDataMatrix =
-            
+    obj = {'meanDataMatrix': None, 'demeanedDataMatrix': None, 'X_a': None, 'X_b': None, 'X_c': None, 'eigvecs': None,
+           'eigvals': None, "covMatrix":None}
+    # De-Meaning the feature space
+    obj['meanDataMatrix'] = dataMatrix.mean(axis=1)
+    obj['demeanedDataMatrix'] = dataMatrix - obj['meanDataMatrix'][:, np.newaxis]
+    dmDataMatrix = obj['demeanedDataMatrix']
+
     # Computing the Covariance 
-    #obj.covMatrix =
-            
+    obj['covMatrix'] = np.dot(dmDataMatrix, dmDataMatrix.T)
+
     # Eigen Value Decomposition
-    
-    
-    
+    w,V = np.linalg.eig(obj['covMatrix'])
+
     # In COV, you need to order the eigevectors according to largest eigenvalues
-    
-    
-    
-    
+    orderedEVal = []
+    orderedEVal.append(w[0])
+    orderedEVal.append(w[2])
+    orderedEVal.append(w[1])
+
+    orderedEVec = np.zeros((3,3))
+    orderedEVec[:,0] = V[:,0]
+    orderedEVec[:,1] = V[:,2]
+    orderedEVec[:,2] = V[:,1]
+
     # Enforce a sign convention on the coefficients -- the largest element (absolute) in each
     # column will have a positive sign.
 
 
     # Compute the accumelative Eigenvalues to finde the desired
     # Variance 
-    
+    sumOfEvals = np.sum(orderedEVal)
+    print("variance of 1st eigen vector : {}".format(orderedEVal[0]/sumOfEvals))
+    print("variance of 2st eigen vector : {}".format(orderedEVal[1]/sumOfEvals))
+    print("variance of 3st eigen vector : {}".format(orderedEVal[2]/sumOfEvals))
+    print("variance of 1st and 2nd eigen vectors : {}".format((orderedEVal[0] + orderedEVal[1])/sumOfEvals))
+
     # Keep the eigenvectors and eigenvalues of the desired
     # variance, i.e. keep the first two eigenvectors and
     # eigenvalues if they have 90% of variance. 
-    #obj.eigvecs =
-    #obj.eigvals =
-            
-    # Project the data 
-    #obj.projectedData =
+    obj['eigvecs'] = orderedEVec
+    obj['eigvals'] = orderedEVal
+
+
+    # Project the data
+    twoPC = orderedEVec[:,0:2]
+    obj['projectedData'] = np.dot(dmDataMatrix.T, twoPC)
     
     # return the object
-    #return obj
+    return obj
--- PCA and KMeans/scriptToyData.py
+++ PCA and KMeans/scriptToyData.py
@@ -10,23 +10,26 @@ data2 = sp.loadmat("data/toydata2.mat")
 
 data = data['D']
 data2 = data2['data']
-obj = myPCA.usingSVD(data, 1)
 
-def visualizeData():
+def visualizeData(obj, label, svdTrans=False ):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
+    fig.set_label(label)
+    fig.text(0.3,0.03, label)
 
     Axes3D.scatter(ax, xs=data[0,:], ys=data[1,:], zs=data[2,:])
     #Axes3D.scatter(ax, xs=data2[0, :], ys=data2[1, :], zs=data2[2, :])
 
-    #transD = obj['X_a'].T                                                  # X_a : all three components
-    transD = obj['X_b'].T                                                   # X_b : first two components
-    #transD = obj['X_c'].T                                                   # X_c : first component only
-    Axes3D.scatter(ax, xs=transD[0,:], ys=transD[1,:], zs=transD[2,:], c="red")
-
-
-    #transD = obj['projectedData'].T
-    #Axes3D.scatter(ax, xs=transD[0,:], ys=transD[1,:], c="red")
+    # This block works for SVD Transformations
+    if svdTrans == True:
+        #transD = obj['X_a'].T                                                   # X_a : all three components
+        transD = obj['X_b'].T                                                   # X_b : first two components
+        #transD = obj['X_c'].T                                                   # X_c : first component only
+        Axes3D.scatter(ax, xs=transD[0,:], ys=transD[1,:], zs=transD[2,:], c="red")
+    else:
+        # This block works for poth SVD and COV
+        transD = obj['projectedData'].T
+        Axes3D.scatter(ax, xs=transD[0,:], ys=transD[1,:], c="red")
 
     kwargs = {'length':3.0, 'pivot':'tail'}
 
@@ -50,5 +53,10 @@ def visualizeData():
 
 
 
+pcaObjSvd = myPCA.usingSVD(data, 1)
+pcaObjCov = myPCA.usingCOV(data,1)
+visualizeData(pcaObjSvd, "PCA with SVD decomposition")
+visualizeData(pcaObjSvd, "PCA with SVD decomposition and transformation", True)
+visualizeData(pcaObjCov, "PCA with Covariance matrix ")
 
-visualizeData()
+
